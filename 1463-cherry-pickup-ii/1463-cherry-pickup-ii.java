9eca01822a0a59b862c71e645be1045f8177a364
Janmesh Singh <68192323+janmeshjs@users.noreply.github.com>
2023-07-12 11:34:42 +0530

    Time: 53 ms (26.03%), Space: 44 MB (82.93%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -4,12 +4,44 @@ class Solution {
         int c = grid[0].length;
         int[][][] dp= new int[r][c][c];
         
-        for(int[][] arr: dp){
-            for(int[] tem: arr){
-                Arrays.fill(tem, -1);
+        // for(int[][] arr: dp){
+        //     for(int[] tem: arr){
+        //         Arrays.fill(tem, -1);
+        //     }
+        // }
+        
+        for(int j1 = 0; j1<c; j1++){
+            for(int j2 = 0; j2<c; j2++){
+                if(j1==j2) dp[r-1][j1][j2]= grid[r-1][j1];
+                else dp[r-1][j1][j2]= grid[r-1][j1]+ grid[r-1][j2];
             }
         }
-        return f(0,0,grid[0].length-1, grid, dp);
+        
+        for(int i =r-2; i>=0; i--){
+            for(int j1 =0; j1<c; j1++){
+                for(int j2 =0; j2<c; j2++){
+                    
+                    int maxi = -100000000;
+                    for(int dj1 = -1; dj1<=1; dj1++){
+                        for(int dj2 = -1; dj2<=1; dj2++){
+                             int value =0;
+                            if(j1==j2) value = grid[i][j1];
+                            else value = grid[i][j1]+grid[i][j2];
+                            if(j1+dj1>=0 && j1+dj1<c && j2+dj2>=0 && j2+dj2<c)
+                                value+= dp[i+1][j1+dj1][j2+dj2];
+                            else 
+                                value+= -100000000;
+                            maxi = Math.max(maxi, value);  
+                                }
+                            }
+                    dp[i][j1][j2]=maxi;
+                         }
+                }
+            }
+        
+        
+        
+        return dp[0][0][c-1];
     }
     
     int f(int i, int j1, int j2, int[][] a, int[][][]dp){
