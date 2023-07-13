9ed08f2c525812b226005e63d981038105861109
Janmesh Singh <68192323+janmeshjs@users.noreply.github.com>
2023-07-13 11:55:11 +0530

    Time: 117 ms (25.20%), Space: 55.6 MB (24.81%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -5,12 +5,29 @@ class Solution {
             sum+=x;
         }
         if(sum%2!=0) return false;
-        
-        Boolean[][] dp= new Boolean[nums.length][sum/2+1];
+        int n = nums.length;
+        int k = sum/2;
+        Boolean[][] dp= new Boolean[nums.length][k+1];
         for(Boolean[] x: dp){
-            Arrays.fill(x,null);
+            Arrays.fill(x,false);
         }
-        return part(nums.length-1, sum/2, nums, dp);
+        for(int i =0; i<n; i++){
+            dp[i][0] = true;
+        }
+        if(nums[0]<=k) dp[0][nums[0]]= true;
+        
+        for(int i =1; i<n; i++){
+            for(int j =1; j<=sum/2; j++){
+                 boolean ntake= dp[i-1][j];
+                    boolean take = false;
+                    if(j-nums[i]>=0){
+                        take = dp[i-1][j-nums[i]];
+                    }
+                    dp[i][j]= take | ntake;
+            }
+        }
+        
+        return dp[n-1][k];
         
         
     }
