25ce6806021a11a1829fd8d1a8849477e7d79ac1
Janmesh Singh <68192323+janmeshjs@users.noreply.github.com>
2023-07-15 13:41:51 +0530

    Time: 8 ms (47.75%), Space: 45.7 MB (45.82%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[][] dp = new int[coins.length][amount+1];
+        // for(int[] arr: dp){
+        //     Arrays.fill(arr, -1);
+        // }
+        for(int i =0; i<=amount; i++){
+            if(i%coins[0]==0)
+            dp[0][i]= 1;
+        }
+        for(int i=1; i<coins.length; i++){
+            for(int j = 0; j<=amount; j++){
+                    int notake = dp[i-1][j];
+                    int take =0;
+                    if(coins[i]<=j){
+                        take = dp[i][j-coins[i]];
+                    }
+                dp[i][j]= take + notake;
+            }
+        }
+       
+        return dp[coins.length-1][amount];
+    }
+    
+    public int coi(int ind, int amount, int[] coins, int[][] dp){
+        if(ind ==0){
+            if(amount%coins[0]==0) return 1;
+            else return 0;
+        }
+        //if(amount==0) return 1;
+        if(dp[ind][amount]!=-1) return dp[ind][amount];
+        int notake = coi(ind-1, amount, coins, dp);
+        int take =0;
+        if(coins[ind]<=amount){
+            take = coi(ind, amount-coins[ind], coins, dp);
+        }
+        
+        return dp[ind][amount] = take+notake;
+    }
+}
