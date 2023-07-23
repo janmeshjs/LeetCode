91b1085581efc2f75547f494ad131d4fbfc12f83
Janmesh Singh <68192323+janmeshjs@users.noreply.github.com>
2023-07-24 01:18:09 +0530

    Time: 16 ms (91.00%), Space: 40.3 MB (98.70%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,18 +1,25 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int[][]dp = new int[s.length()][t.length()];
-        for(int[] arr: dp){
-            Arrays.fill(arr, -1);
+        
+        int[] prev = new int[t.length()+1];
+        prev[0]=1;
+        
+        for(int i =1; i<=s.length(); i++){
+            for(int j =t.length(); j>=1; j--){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                     prev[j] = prev[j-1] + prev[j];
+                }
+            }
         }
-        return f(s.length()-1, t.length()-1, s, t, dp);
+        return prev[t.length()];
     }
     
     public int f(int i, int j, String s, String t, int[][] dp){
-        if(j<0) return 1;
-        if(i<0) return 0;
+        if(j==0) return 1;
+        if(i==0) return 0;
         
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s.charAt(i)==t.charAt(j)){
+        if(s.charAt(i-1)==t.charAt(j-1)){
             return dp[i][j] = f(i-1, j-1, s, t, dp) + f(i-1, j, s, t, dp);
         }
         else 
