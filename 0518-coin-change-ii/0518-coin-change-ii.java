32c3439de697889d331ba4ef9a2fe3a7b3b1789e
Janmesh Singh <68192323+janmeshjs@users.noreply.github.com>
2023-07-28 09:45:59 +0530

    Time: 9 ms (36.54%), Space: 46.4 MB (9.49%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,44 +1,43 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        //int[][] dp = new int[coins.length][amount+1];
-        // for(int[] arr: dp){
-        //     Arrays.fill(arr, -1);
-        // }
-        int[] prev = new int[amount+1];
-        
+        int[][] dp = new int[coins.length+1][amount+1];
+        for(int[] arr: dp){
+            Arrays.fill(arr, -1);
+        }
         for(int i =0; i<=amount; i++){
-            if(i%coins[0]==0)
-            prev[i]= 1;
+            if(i%coins[0]==0) dp[0][i] = 1;
+            else dp[0][i]=0;
         }
+        
         for(int i=1; i<coins.length; i++){
-            int[] curr = new int[amount+1];
             for(int j = 0; j<=amount; j++){
-                    int notake = prev[j];
-                    int take =0;
-                    if(coins[i]<=j){
-                        take = curr[j-coins[i]];
-                    }
-                curr[j]= take + notake;
+                int notake = dp[i -1][j];
+                int take =0;
+                if(coins[i]<=j){
+                    take = dp[i][j-coins[i]];
+                }
+
+                 dp[i][j] = take + notake;
             }
-            prev = curr;
         }
-       
-        return prev[amount];
+        
+        return dp[coins.length-1][amount];
     }
-    
-    public int coi(int ind, int amount, int[] coins, int[][] dp){
-        if(ind ==0){
-            if(amount%coins[0]==0) return 1;
-            else return 0;
+    public int f(int ind, int amount, int[] coins, int[][] dp){
+        if(ind==0){
+            if(amount%coins[0]==0)return 1;
+             else return 0;
         }
-        //if(amount==0) return 1;
         if(dp[ind][amount]!=-1) return dp[ind][amount];
-        int notake = coi(ind-1, amount, coins, dp);
+        int notake = f(ind -1, amount, coins, dp);
         int take =0;
         if(coins[ind]<=amount){
-            take = coi(ind, amount-coins[ind], coins, dp);
+            take = f(ind, amount-coins[ind], coins, dp);
         }
         
-        return dp[ind][amount] = take+notake;
+        return dp[ind][amount] = take + notake;
     }
 }
+
+
+
