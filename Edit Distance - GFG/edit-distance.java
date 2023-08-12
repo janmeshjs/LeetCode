6c6d2f3f993ef37fb991d19c6d2d09d916110fab
Janmesh Singh <68192323+janmeshjs@users.noreply.github.com>
2023-08-12 23:32:58 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.java
+++ Edit Distance - GFG/edit-distance.java
@@ -25,9 +25,19 @@ class GFG {
 class Solution {
     public int editDistance(String s, String t) {
         // Code here
-        int[][] dp = new int[s.length()][t.length()];
-        for(int[] arr: dp)Arrays.fill(arr, -1);
-        return edit(s.length()-1, t.length()-1, s, t, dp);
+        int[][] dp = new int[s.length()+1][t.length()+1];
+
+        for(int i=0; i<=s.length(); i++){
+         for(int j=0; j<=t.length(); j++){
+                if(i==0) dp[i][j] = j;
+                else if(j==0) dp[i][j] = i;
+                
+                else if(s.charAt(i-1)==t.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
+                
+                else dp[i][j]= 1+ Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+            }
+        }
+        return dp[s.length()][t.length()];
     }
     
     public int edit(int i, int j, String s, String t, int[][] dp) {
