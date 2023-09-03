466acbae4ef38962cfdd87b14ba1a46b2443cc83
Janmesh Singh <68192323+janmeshjs@users.noreply.github.com>
2023-09-03 18:53:14 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (8.49%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,39 +1,19 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-//        int[][] dp =new int[m][n];
-//         for(int[] arr: dp){
-//             Arrays.fill(arr, -1);
-//         }
-//         return help(m-1, n-1, dp);
-//     }
-    
-//     public int help(int m, int n, int[][] dp){
-//          if(m==0 && n==0) dp[m][n]=1;
-//         if(m<0 || n<0) return 0;
+        int[][] dp= new int[m+1][n+1];
+        for(int[] arr: dp) Arrays.fill(arr, -1);
+        return uni(0,0,m,n, dp);
+    }
+    public int uni(int r, int c, int m, int n, int[][] dp){
+        if(r>=m || c>=n)return 0;
+        if(r==m-1 && c == n-1) return 1;
+        if(dp[r][c]!=-1) return dp[r][c];
+        int right = 0;
+        int down =0;
+        right = uni(r, c+1, m, n, dp);
+        down = uni(r+1, c, m, n, dp);
         
-//         if(dp[m][n]!=-1) return dp[m][n];
-     
-//         return dp[m][n]= help(m-1,n, dp) + help(m,n-1, dp);
-//     }
-        
-        // Tabulation
-        
-     int[][] dp = new int[m][n];
-        dp[0][0]= 1;
-        for(int i = 0; i<m; i++){
-            for(int j =0; j<n; j++){
-                if(i==0 && j==0) dp[i][j]=1;
-                else{
-                    int up=0, dow=0;
-                if(i>0) up =dp[i-1][j];
-                if(j>0) dow= dp[i][j-1];
-                
-                dp[i][j]= up+dow;
-                }
-            }
-        }
-        
-        return dp[m-1][n-1];
+        return dp[r][c]=right +down;
         
     }
 }
